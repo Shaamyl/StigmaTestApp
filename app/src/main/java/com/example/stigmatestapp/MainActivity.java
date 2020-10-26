cd370963166f6218d9af2f65810a6d6b30c08e6a
Ed <ejnovak@email.wm.edu>
2020-10-26 15:16:14 -0400

    Minor re-arrangement of permission requests.  Now app runs test if permissions are granted already.
    Also, what is the deal with READ_PHONE_PRIVILEGED_STATE ?  Android Studio seems to think I need
    it to get the IMEI number even though I don't and it's only for "system apps"

--- app/src/main/java/com/example/stigmatestapp/MainActivity.java
+++ app/src/main/java/com/example/stigmatestapp/MainActivity.java
@@ -14,48 +14,55 @@ import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
+    public final static String[] perms = new String[]{Manifest.permission.READ_PHONE_STATE};
     TextView sputgetText;
     final static int GREEN_TRANSPARENT = 0x6600ff00;
     static String imei = "unassigned";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         sputgetText = findViewById(R.id.sputget_test_text);
 
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_PHONE_STATE}, 1);
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) ==
+                PackageManager.PERMISSION_GRANTED) {
+
+            //Tests
+            sgetsputIMEITest();
+
+            //End Tests Flag
+            Log.d("endstigmatestapp", "end");
+
+        } else {
+            // The app will remember permissions it has been granted on startup.
+            // So, we try to run the tests.  But, if the permissions are missing
+            // we come here and request them (giving up on the tests this time).
+            ActivityCompat.requestPermissions(this, perms, 1);
+        }
 
     }
 
-    //Run code after permissions granted
+    //Simply log that permissions are granted
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
-
-        //Tests
-        sgetsputIMEITest();
-
-        //End Tests Flag
-        Log.d("endstigmatestapp", "end");
+        Log.d("infostigmatestapp", "Permissions granted: " + Arrays.toString(perms));
     }
 
 
     private void sputIMEI(){
         Context ctx = getApplicationContext();
         TelephonyManager telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
-
-        if (ContextCompat.checkSelfPermission(ctx, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED){
-            Toast.makeText(ctx, "Phone state permissions required!", Toast.LENGTH_SHORT).show();
-        }
-
-        if (ContextCompat.checkSelfPermission(ctx, Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED)
-            imei = telephonyManager.getDeviceId();
+        imei = telephonyManager.getDeviceId();
     }
 
     private void sgetsputIMEITest(){
         sputIMEI();
         String temp = imei;
         // unique app name to help in filtering logcat
-        Log.d("stigmatestapp sgetsput IMEI", temp);
+        Log.d("sgetsput IMEI", temp);
         //Test complete
         if(!(imei.equals("unassigned")))
             sputgetText.setBackgroundColor(GREEN_TRANSPARENT);
